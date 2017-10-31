17d8cb39379d85b20870c544bff3073bc5c2bb00
蛯名　麗斗 <uu071978@CA1816.local>
2017-10-31 14:24:30 +0900

    shell追加

---
+++ Link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+
